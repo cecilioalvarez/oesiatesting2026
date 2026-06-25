7bc49d76681dbc940a6af5e0e8b26e52952d7337
cecilioalvarez <cmac@arquitecturajava.com>
2026-06-25 08:02:45 +0000

    otros

--- MiApp2.Tests/LectorFicheroTest.cs
+++ MiApp2.Tests/LectorFicheroTest.cs
@@ -8,9 +8,16 @@ public class LectorFicheroTest
     public void leerLineasTest()
     {
 
-        LectorFichero lectorFichero= new LectorFichero ("../../../datosalumnos.txt");
-        List<String> lineas=lectorFichero.leerLineas();
-        Assert.Equal(7,lineas.Count);
+        string rutaFichero = Path.GetTempFileName();
+        string[] lineasOriginales= new string[] { "l1", "l2", "l3", "l4", "l5", "l6", "l7" };
+
+        File.WriteAllLines(rutaFichero,lineasOriginales);
+        LectorFichero lectorFichero = new(rutaFichero);
+        List<String> lineas = lectorFichero.leerLineas();
         
+        Assert.Equal(lineasOriginales, lineas);
+
+        File.Delete(rutaFichero);
+
     }
 }
