b0b553eb5f14544dae045e6e288b8bc4bcd16ff0
cecilioalvarez <cmac@arquitecturajava.com>
2026-06-25 08:13:07 +0000

    otros

---
+++ MiApp2.Tests/AlumnoTest.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+using Xunit;
+using MiApp2;
+
+namespace MiApp2.test;
+
+
+// FIRST FAST ISOLATEd REPETIBLE, SELF VALIDATING ,TIMELY 
+public class AlumnoTest
+{
+    
+
+    [Fact]
+    public void Alumno_AgregarNota_Test()
+    {
+        // arrange preparar 
+        var alumno = new Alumno(1, "pedro", new List<Nota>());
+        var nota = new Nota(7);
+
+        //assert guard y sirve para validar antes de actuar que los datos iniciales son ok
+        Assert.Empty(alumno.Notas);
+
+        alumno.AgregarNota(nota);
+
+        //assert validar
+        Assert.Contains(nota, alumno.Notas);
+        Assert.Single(alumno.Notas);
+
+    }
+
+    [Fact]
+    public void EliminarNota_CuandoNotaExiste_LaEliminaDeLaLista()
+    {
+        Nota nota = new Nota(8);
+        var alumno = new Alumno(1, "Juan", new List<Nota> { nota });
+        // Act
+        alumno.EliminarNota(nota);
+
+        // Assert
+        Assert.DoesNotContain(nota, alumno.Notas);
+    }
+
+
+
+
+
+
+}
---
+++ MiApp2.Tests/NotaTest.cs
@@ -0,0 +1,59 @@
+using System.Reflection;
+using Xunit;
+using MiApp2;
+
+namespace MiApp2.test;
+
+// FIRST
+
+// FAST , ISOLATED, REPETABLES,Selft Validating, Timely
+
+public class NotaTest
+{
+    [Fact]
+    public void Nota_Aprobada_Test()
+    {
+        // arrange preparar 
+
+        var nota = new Nota(5.5);
+        var calificacion= nota.ObtenerCalificacion();
+        Assert.Equal("Aprobado",calificacion);
+    }
+
+   [Fact]
+    public void Nota_SobreSaliente_Test()
+    {
+        var nota = new Nota(9.5);
+        var calificacion= nota.ObtenerCalificacion();
+        Assert.Equal("Sobresaliente",calificacion);
+    }
+
+    //boundaries los limites entonces a mi me interesa tener un test
+     [Fact]
+    public void Nota_SobreSaliente9_Test()
+    {
+        var nota = new Nota(9);
+        var calificacion= nota.ObtenerCalificacion();
+        Assert.Equal("Sobresaliente",calificacion);
+    }
+
+     [Fact]
+    public void Nota_SobreSaliente10_Test()
+    {
+        var nota = new Nota(10);
+        var calificacion= nota.ObtenerCalificacion();
+        Assert.Equal("Sobresaliente",calificacion);
+    }
+    
+    [Fact]
+      public void Nota_Mayor_10_Test()
+    {
+  
+        var ex = Assert.Throws<ArgumentOutOfRangeException>(() =>  new Nota(10.1));
+        Assert.Contains("El valor debe estar entre 0 y 10", ex.Message);
+    }
+
+
+    
+
+}
